392e6c9978f5e4cb9fd1e269938d98195546d991
bkataru <baalateja.k@gmail.com>
2026-02-01 13:40:57 +0530

    chore: remove unused vendor files, scripts, and static SVGs

--- vendors/github-readme-activity-graph/replit.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs }: {
-	deps = [
-        pkgs.nodejs-16_x
-        pkgs.nodePackages.typescript-language-server
-        pkgs.nodePackages.nodemon
-        pkgs.nodePackages.yarn
-        pkgs.replitPackages.jest
-	];
-}
